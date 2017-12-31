d37d130657529da3b2e554c7f265eff7663ede2e
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2017-12-31 14:44:36 +0900

    divide

--- Firebase.ino
+++ Firebase.ino
@@ -20,32 +20,31 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-// Set these to run example.
-#define FIREBASE_HOST "tahosook-smarthome.firebaseio.com"
-#define FIREBASE_AUTH "AAAA0xo5Q5M:APA91bFo-1dws_FNvxdDM1WdXqKZgvsvwPTm_e6Xw0mL5XHc7RB4MzXSE226JmAuFgrlZPqkZgxRohx1xvc_fVOFHSuRfmEGAanxouFrzLpCdaScRJRn_f5mdbBaVPCrGkUJW6VsR176"
-#define WIFI_SSID "30F772BBF363-2G"
-#define WIFI_PASSWORD "2215092608712"
-
-void initWifi() {
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
+void initWifi()
+{
+    // connect to wifi.
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
 }
-void initFirebase() {
-  //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.begin(FIREBASE_HOST);
+void initFirebase()
+{
+    //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Firebase.begin(FIREBASE_HOST);
 }
 
-String readFirebase(){
+String readFirebase()
+{
     String word = Firebase.getString("googlehome/word");
-    if (Firebase.failed()) {
+    if (Firebase.failed())
+    {
         Serial.print("getting /googlehome/word failed:");
         Serial.println(Firebase.error());
         return "error";
@@ -57,9 +56,11 @@ String readFirebase(){
     return word;
 }
 
-void resetFirebase(){
-    Firebase.setString("googlehome/word","");
-    if (Firebase.failed()) {
+void resetFirebase()
+{
+    Firebase.setString("googlehome/word", "");
+    if (Firebase.failed())
+    {
         Serial.print("getting /googlehome/word failed:");
         Serial.println(Firebase.error());
     }
